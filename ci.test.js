979619b36c1ea706f949648e7de4571b0d6e7fe1
Kevin Garner <kevin.garner@ssigroup.com>
2021-11-09 08:37:42 -0600

    Add first CI steps

--- ci.test.js
+++ ci.test.js
@@ -1,18 +1,30 @@
-var fs = require('fs'); 
+var fs = require('fs');
 
 describe('CI sequence', () => {
-
   var fileContents;
   beforeEach(() => {
     fileContents = fs.readFileSync('ci.md', 'utf8');
   });
 
   it('the list has a header', () => {
-    expect(/.*#.*/ig.test(fileContents)).toBe(true);
+    expect(/.*#.*/gi.test(fileContents)).toBe(true);
   });
 
   // TODO add the tests between these comments =>
+  it('1. pull latest code', () => {
+    expect(/.*pull.*/gi.test(fileContents)).toBe(true);
+  });
 
+  it('2. add commits', () => {
+    expect(/.*commit.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it('3. push to the remote branch with the same name', () => {
+    expect(/.*push.*/gi.test(fileContents)).toBe(true);
+  });
+
+  it('4. open a pull request and continue working', () => {
+    expect(/.*pull\s+request.*/gi.test(fileContents)).toBe(true);
+  });
   // TODO <= add the tests between these comments
-
 });
